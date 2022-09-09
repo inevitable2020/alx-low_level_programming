5ebf08d60e96efa2dab64bc3c6bcd2a573b4ba97
inevitable2020 <afolabiayotomiwa8@gmail.com>
2022-09-09 15:43:54 -0700

    c-programming language

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-/*
- * Main entry point
+
+/**
  *
- * return 0
+ *  * main - Entry point
  *
- */
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
