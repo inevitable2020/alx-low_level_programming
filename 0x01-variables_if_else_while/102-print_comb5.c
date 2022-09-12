c26f2f649a08fcebf1626a28cff238731150fa5e
inevitable2020 <afolabiayotomiwa8@gmail.com>
2022-09-12 09:28:41 -0700

    print

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * moain - prints all possible combinations of two two-digits numbers
+ * main - prints all possible combinations of two two-digits numbers
  * Return: Always 0.
  */
 int main(void)
