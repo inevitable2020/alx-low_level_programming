31853fa610e7d48fb597792596cc01f51a6807d3
inevitable2020 <afolabiayotomiwa8@gmail.com>
2022-09-12 08:17:44 -0700

    print

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowerase.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+	char l;
+
+	for (n = 0; n <10; Nn++)
+		putchar((n % 10) + '0');
+
+	for (l = 'a'; l <= 'f'; l++)
+		putchar(l);
+
+	putchar('\n');
+	return (0);
+}
