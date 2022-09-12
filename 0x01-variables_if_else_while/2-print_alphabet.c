f2074811fa3495037398be1f078e35013c83a39b
inevitable2020 <afolabiayotomiwa8@gmail.com>
2022-09-12 07:32:21 -0700

    positive

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
+
 /**
-*
-* main - Prints the alphabet in lowercase.
-*
-*
-* Return: Always 0.
-*/
+ * main - Prints the alphabet in lowercase.
+ * followed by a new line
+ * Return: Always 0.
+ */
 int main(void)
 {
 	char letter;
