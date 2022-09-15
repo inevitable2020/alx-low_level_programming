0e8147d418d50b535d1727838f04f5056301bc37
inevitable2020 <afolabiayotomiwa8@gmail.com>
2022-09-15 12:25:25 -0700

    function

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,10 +7,5 @@
  */
 int _isupper(int c)
 {
-	char c;
-	c = 'C';
-	printf("Return value when uppercase character %c is passed to isupper(): %d", c, isupper(c));
-	c = '+';
-	printf("\nReturn value when another character %c is passed to is isupper(): %d", c, isupper(c));
-	return 0;
+	return (c >= 'A' && c <= 'Z');
 }
