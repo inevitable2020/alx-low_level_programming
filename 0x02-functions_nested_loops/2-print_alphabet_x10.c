b915cb906550a4b1e539e605a6e8805447dca603
inevitable2020 <afolabiayotomiwa8@gmail.com>
2022-09-17 12:55:35 -0700

    function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10  - function to print abc ten times
+ * You can use only put char
+ * Return: nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	char a, b;
+
+	for (b = 0; b < 10; b++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+		_putchar(a);
+		}
+	_putchar('\n');
+	}
+}
