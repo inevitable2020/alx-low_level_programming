33b463646949b62f191c6de8a9e9ad734fcdfb59
inevitable2020 <afolabiayotomiwa8@gmail.com>
2022-09-18 12:33:57 -0700

    nested

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,8 +19,7 @@ void print_times_table(int n)
 				{
 					_putchar(k + '0');
 				}
-				else if
-					(k < 10 && j != 0)
+				else if (k < 10 && j != 0)
 					{
 						_putchar(',');
 						_putchar(' ');
@@ -28,8 +27,7 @@ void print_times_table(int n)
 						_putchar(' ');
 						_putchar(k + '0');
 					}
-				else if
-					(k >= 10 && k < 100)
+				else if (k >= 10 && k < 100)
 					{
 						_putchar(',');
 						_putchar(' ');
@@ -37,8 +35,7 @@ void print_times_table(int n)
 						_putchar((k / 10) + '0');
 						_putchar((k % 10) + '0');
 					}
-				else if
-					(k >= 100)
+				else if (k >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
