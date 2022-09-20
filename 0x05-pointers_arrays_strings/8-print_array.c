e531e5e7c58d3e68b7e447d1733f819586943d09
inevitable2020 <afolabiayotomiwa8@gmail.com>
2022-09-20 13:47:37 -0700

    arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - unction that prints n elements of an array of integers
+ *
+ * @a: pointer to first int
+ * @n: pointer to second int
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+	}
+	printf("\n");
+}
