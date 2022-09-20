a8b92185db10c5bd30fa82979ecbf84a95fa813f
inevitable2020 <afolabiayotomiwa8@gmail.com>
2022-09-20 07:12:48 -0700

    function

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,9 @@
 #include "main..h"
 
 /**
- * puts_half - function that prints half of a string
- *
- * @str: pointer to char
- * Return: nothing
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
  */
 void puts_half(char *str)
 {
