bd57434ad5364c9e93952868fe08cddd92afdd53
inevitable2020 <afolabiayotomiwa8@gmail.com>
2022-09-27 13:27:48 -0700

    pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,11 +6,12 @@
  * @b: character to be used
  * @n: unsigned integer
  * Return: Always 0
+ *
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
