6899701118ac88941fa1874019c645083e9411de
inevitable2020 <afolabiayotomiwa8@gmail.com>
2022-09-28 13:52:21 -0700

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,25 @@
+/*
+ * File: 2-strien_recursion.c
+ * Auth: Afolabi Ayotomiwa Matthew
+ */
+
+#include "main.h"
+
+/**
+ * _strien_recursion - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int_strien_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strien_recursion(s + 1);
+	}
+
+	return (len);
+}
