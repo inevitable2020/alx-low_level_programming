3b276279839d727681aa7d19d6db5646a2f0cbd5
inevitable2020 <afolabiayotomiwa8@gmail.com>
2022-10-10 13:50:34 -0700

    preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * File: 0-object_like_macro.h
+ * Description: Create a header file that defines a macro named SIZE
+ * and the abbreviation for the token to be 1024.
+ */
+#define SIZE 1024
+
+#endif
