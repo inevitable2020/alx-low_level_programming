c2286abd73019aa078be11fef46e02598e8c31f2
inevitable2020 <afolabiayotomiwa8@gmail.com>
2022-10-10 13:52:35 -0700

    preprocessor

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,11 @@
+#ifndef PI_H
+#define PI_H
+
+/**
+ * File: 1-pi.h
+ * Description: Create a header file that defines a macro named PI
+ * and the abbreviation of the token to be  3.14159265359
+ */
+#define PI 3.14159265359
+
+#endif
