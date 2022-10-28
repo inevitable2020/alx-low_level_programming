bcbe8cfef7a0061de509043eee8efa75a7057421
inevitable2020 <afolabiayotomiwa8@gmail.com>
2022-10-28 07:34:09 -0700

    bit

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,14 +3,25 @@
 /**
  * get_endianness - a func that checks endianness
  * Return: 0 if big endian, 1 if little endian
+ * This is an attribute of a systme that indicates whether integers are
+ * represented with the most significant byte stored at the lowest address
+ * (big endian) or at the highest address (littles endian).
  */
 
 int get_endianness(void)
 {
-	unsigned int j = 1;
-	char *c = (char *)&j;
+	int rand_num = 100;
+	char *ptr = (char *) &rand_num;
+	/**
+	 * Ava estamos casteando la dir del int rand_num a un string (char *)
+	 */
 
-	if (*c)
-		return (1);
-	return (0);
+	if (*ptr != 0)
+	{
+		return (1); /* Si es distinto a cero es el little endian */
+	}
+	else
+	{
+		return (0);
+	}
 }
